dca2e170255b9eb42cc09d8a041917c925f376e2
DavidStGS <davidgarcia200124@gmail.com>
2023-08-26 19:40:09 -0500

    Pagina terminada

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("a").click(function () {
+        $("ul").css("background-color", "#309bff");
+        $("footer").css("background-color", "#309bff");
+    });
+});
